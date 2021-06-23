b4d875520bcfd484524158967293c11da19f62f4
Shubham Agrawal <shub8968@gmail.com>
2021-06-23 21:01:10 +0530

    code change

--- src/main/java/nl/hans/service/MyPlugin2.java
+++ src/main/java/nl/hans/service/MyPlugin2.java
@@ -3,13 +3,19 @@ package nl.hans.service;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.SimplyTimed;
 
-public class MyPlugin2 extends BasePlugin {
+import javax.inject.Inject;
+
+public class MyPlugin2 implements Plugin{
+
+  @Inject
+  MyBean myBean;
   public String getName() {
     return "my-plugin-2";
   }
 
+
   @SimplyTimed(unit = MetricUnits.MILLISECONDS)
-  public String run() {
+  public String execute() {
     myBean.run();
     return "This is plugin Two";
   }
